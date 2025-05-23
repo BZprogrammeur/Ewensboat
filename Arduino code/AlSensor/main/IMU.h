8c7ad199c3e29264e149d5ddf40614c527759e2c
Noé LEMAISTRE <noe.lemaistre@ensta.fr>
2025-05-23 13:10:45 +0200

    POO

--- Arduino code/AlSensor/main/IMU.h
+++ Arduino code/AlSensor/main/IMU.h
@@ -4,8 +4,25 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-void imu_init();
-void get_all_IMU_data();
-float get_cap();
+class IMU {
+public:
+    IMU();
+    void init();
+    void update();
+    float get_cap();
+
+private:
+    unsigned char angle8;
+    unsigned char high_byte;
+    unsigned char low_byte;
+    char pitch;
+    char roll;
+    unsigned int angle16;
+    double cap;
+
+
+    static const int CMPS12_ADDRESS = 0x60; // Adresse I2C du CMPS12
+    static const int ANGLE_8 = 0x00;        // Registre pour l'angle 8 bits
+};
 
 #endif 
